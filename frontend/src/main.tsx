6d3411aab2447a00fcec08c87239d1c2f6448440
Hakkenheiner <remi.zicken@gmail.com>
2024-11-04 19:25:16 +0100

    create rooter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,25 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import Budget from "./pages/Budget.tsx";
+import Home from "./pages/Home.tsx";
+import "./styles/index.css";
+
+const rooter = createBrowserRouter([
+	{
+		element: <App />,
+		children: [
+			{
+				path: "/Dashboard",
+				element: <Home />,
+			},
+			{
+				path: "/Budget",
+				element: <Budget />,
+			},
+		],
+	},
+]);
 
 const root = document.getElementById("root");
-root &&
-	createRoot(root).render(
-		<StrictMode>
-			<App />
-		</StrictMode>,
-	);
+root && createRoot(root).render(<RouterProvider router={rooter} />);
